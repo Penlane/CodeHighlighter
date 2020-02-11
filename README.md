67c1c0d2315c7137a5b86dc11ac0cd88fd6990f2
Penlane <penlane@protonmail.com>
2020-02-11 13:05:17 +0100

    Make filename customizable via input param

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ Highlights the code of a specified language using pygments in node.js
 * Make sure you've got at least node.js v10 and python3 with pip installed
 * Install Pygments via pip install Pygments
 * Clone repo and use "npm i" in the directory\
-* Run _node ./src/index.js cpp "directory/to/mySource"_ (script, lexer, input/output directory - separated by space)
+* Run _node ./src/index.js cpp "directory/to/mySource" myOutputFileName_ (script, lexer, input/output directory, desired output Filename - separated by space)
 * The output will be created in the given "directory/to/mySource"
--- src/index.js
+++ src/index.js
@@ -2,8 +2,26 @@ const fs = require('fs');
 
 const lexer = process.argv[2];
 const startDirectory = process.argv[3];
+const fileName = process.argv[4];
+
 console.log(`Lexer: ${lexer}`);
 console.log(`startDir: ${startDirectory}`);
+console.log(`Desired filename: ${fileName}`);
+
+if(typeof fileName === 'undefined') {
+    console.log('Enter correct filename');
+    return;
+}
+
+if(typeof lexer === 'undefined') {
+    console.log('Enter correct lexer');
+    return;
+}
+
+if(typeof startDirectory === 'undefined') {
+    console.log('Enter correct startDirectory');
+    return;
+}
 
 let massiveString;
 console.log(`Command: ${`cat '${startDirectory}\\*'`}`);
@@ -12,12 +30,12 @@ fs.readdir(startDirectory, (err, files) => {
         massiveString+=require('child_process').execSync(`cat '${startDirectory}\\${file}'`).toString('UTF-8');
     }
     // console.log(massiveString);
-    fs.writeFile(`${startDirectory}\\summary.cpp`, massiveString, (err) => {
+    fs.writeFile(`${startDirectory}\\${fileName}.cpp`, massiveString, (err) => {
         if(err) {
             console.log(`Error when creating the merged cpp file: ${err}`);
             return;
         } else {
-            require('child_process').execSync(`pygmentize -f html -l ${lexer} -o "${startDirectory}\\summary.html" "${startDirectory}\\summary.cpp"`);
+            require('child_process').execSync(`pygmentize -f html -l ${lexer} -o "${startDirectory}\\${fileName}.html" "${startDirectory}\\${fileName}.cpp"`);
             console.log('Done...check directory');
         }
     });
